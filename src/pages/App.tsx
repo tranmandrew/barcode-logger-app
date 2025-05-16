b60453dcc8d8a0f0bc91c56894ec4c89be9263c2
tranmandrew <andersontran79@gmail.com>
2025-05-16 11:26:45 -0500

    Add BrowserRouter and /dashboard route

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScanPage from "./ScanPage";
+import DashboardPage from "./Dashboard";
 
 export default function App() {
   return (
-    <div className="p-4 max-w-md mx-auto">
-      <ScanPage />
-    </div>
+    <BrowserRouter>
+      <div className="p-4 max-w-md mx-auto">
+        <Routes>
+          <Route path="/" element={<ScanPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
